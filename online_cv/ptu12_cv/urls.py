734d24be10c738411c93964ff250568f58632c77
DariusKaseta <kasetadarius@gmail.com>
2023-06-16 14:00:03 +0300

    views added for cv_detail, cv_participle_views

--- online_cv/ptu12_cv/urls.py
+++ online_cv/ptu12_cv/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+# from .views import cv_participles_view
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cv/<int:pk>/", views.cv_detail, name="cv_details"),
+    path("participle/", views.cv_participles_view, name="participle_view"),
 ]
--- online_cv/ptu12_cv/views.py
+++ online_cv/ptu12_cv/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import CV
 
 
 def index(request):
@@ -8,3 +9,13 @@ def index(request):
 
 def base_view(request):
     return render(request, 'ptu12_cv/base.html')
+
+
+def cv_detail(request, pk):
+    cv = get_object_or_404(CV, pk=pk)
+    return render(request, "ptu12_cv/cv_details.html", {"cv": cv})
+
+
+def cv_participles_view(request):
+    participles = CV.objects.all()
+    return render(request, 'ptu12_cv/cv_participles_view.html', {"participles": participles})
