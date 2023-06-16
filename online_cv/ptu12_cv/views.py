7622be87468d29319da7236e3b719210e08bc4ca
DariusKaseta <kasetadarius@gmail.com>
2023-06-16 10:25:36 +0300

    index view

--- online_cv/ptu12_cv/views.py
+++ online_cv/ptu12_cv/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "ptu12_cv/index.html" )
+
+
+def base_view(request):
+    return render(request, 'ptu12_cv/base.html')
