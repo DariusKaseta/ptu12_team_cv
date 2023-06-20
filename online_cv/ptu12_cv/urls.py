5bebf0ec7441e0af123179f3f3cd192e8ac8eaf7
Petras Anskaitis <sartep1996@gmail.com>
2023-06-20 13:24:41 +0300

    fixed pdf

--- online_cv/ptu12_cv/urls.py
+++ online_cv/ptu12_cv/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('CVs/', views.cv_list, name='cvs'),
     path("cv/<int:pk>/", views.cv_detail, name="cv_details"),
     path("participle/", views.cv_participles_view, name="participle_view"),
-    path('cv/<int:cv_id>/generate-pdf/', views.cv_pdf_view, name='cv_details_pdf.html'),
-    path('CVs/update/', views.update_cv, name='update_cv'),
+    path('cv/<int:cv_id>/generate-pdf/', views.cv_pdf_view, name='cv_details_pdf'),
+    # path('CVs/update/', views.update_cv, name='update_cv'),
 
 
     
