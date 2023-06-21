678b55e57302c51807e1dd24d0df5ec94d63023b
Milda Auglyte <auglyte.m@gmail.com>
2023-06-21 11:26:46 +0300

    meeting reservation completed

--- online_cv/meeting_reservation/views.py
+++ online_cv/meeting_reservation/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import MeetingReservation
 from .forms import MeetingReservationForm
-from .forms import MeetingReservationForm
-from django.contrib.auth.decorators import login_required
+from .models import HRRepresentative
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 
 def meeting_reservation_list(request):
     reservations = MeetingReservation.objects.all()
-    return render(request, 'meeting_reservation/meeting_reservation_list.html', {'reservations': reservations, 'user': request.user})
+    return render(request, 'meeting_reservation/meeting_reservation_list.html', {'reservations': reservations})
 
-@login_required
-def create_meeting_reservation(request):
+def create_meeting_reservation(request, cv_user_id):
+    cv_user = get_object_or_404(User, id=cv_user_id)
+    
     if request.method == 'POST':
-        form = MeetingReservationForm(request.user, request.POST)
+        form = MeetingReservationForm(request.POST, current_user=request.user, cv_user=cv_user)
         if form.is_valid():
             meeting_reservation = form.save(commit=False)
-            meeting_reservation.user = request.user
+            hr_representative = get_object_or_404(HRRepresentative, user=request.user)
+            meeting_reservation.hr_representative = hr_representative
             meeting_reservation.save()
+            meeting_reservation.parties.add(cv_user)
+            meeting_reservation.parties.add(hr_representative.user)
+            meeting_reservation.save()
+
             return redirect('meeting_reservation_list')
     else:
-        form = MeetingReservationForm(request.user)
-    return render(request, 'meeting_reservation/create_meeting_reservation.html', {'form': form})
+        form = MeetingReservationForm(current_user=request.user, cv_user=cv_user)
+    
+    return render(request, 'meeting_reservation/create_meeting_reservation.html', {'form': form, 'cv_user': cv_user})
--- online_cv/ptu12_cv/urls.py
+++ online_cv/ptu12_cv/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import cv_pdf_view, cv_participles_view
+# from .views import cv_pdf_view, cv_participles_view
 
 
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('CVs/', views.cv_list, name='cvs'),
     path("cv/<int:pk>/", views.cv_detail, name="cv_details"),
     path("participle/", views.cv_participles_view, name="participle_view"),
-    path('cv/<int:cv_id>/generate-pdf/', views.cv_pdf_view, name='cv_details_pdf'),
+    # path('cv/<int:cv_id>/generate-pdf/', views.cv_pdf_view, name='cv_details_pdf'),
     # path('CVs/update/', views.update_cv, name='update_cv'),
 
 
