4a9a0e787ff9e03e506b23a671c55b95301e2f06
Milda Auglyte <auglyte.m@gmail.com>
2023-06-21 08:39:13 +0300

    meeting reservation

--- online_cv/meeting_reservation/forms.py
+++ online_cv/meeting_reservation/forms.py
@@ -2,18 +2,18 @@ from django import forms
 from .models import MeetingReservation
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
-
 
 class DateTimeInput(forms.DateTimeInput):
     input_type = 'datetime-local'
-
 
 class MeetingReservationForm(forms.ModelForm):
     def __init__(self, current_user, *args, **kwargs):
         super(MeetingReservationForm, self).__init__(*args, **kwargs)
         self.fields['hr_representative'].queryset = User.objects.filter(username=current_user)
+        self.fields['parties'].choices = [(current_user.pk, current_user.get_full_name()),
+                                          (self.fields['hr_representative'].queryset.first().pk,
+                                           self.fields['hr_representative'].queryset.first().get_full_name())]
 
     class Meta:
         model = MeetingReservation
