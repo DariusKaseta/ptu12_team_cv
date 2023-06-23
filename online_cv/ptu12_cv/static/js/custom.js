03b3094b5004468dab01734ba9c61fb7219de5fb
DariusKaseta <kasetadarius@gmail.com>
2023-06-23 08:42:06 +0300

    cosmetic repair

--- online_cv/ptu12_cv/static/js/custom.js
+++ online_cv/ptu12_cv/static/js/custom.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
     newEducationForm.find('input').val('');
     newEducationForm.find('select').val('');
     
-    // Generate a unique prefix for the new formset
-    var newPrefix = 'education-' + ($('.education-form').length + 1);
+    
+    var newIndex = $('.education-form').length;
+      
     newEducationForm.find('input, select').each(function() {
     var name = $(this).attr('name');
-    if (name) {
-        // Update the name attribute with the new prefix
-        $(this).attr('name', name.replace('education-0', newPrefix));
+        if (name) {
+          
+        $(this).attr('name', name.replace(/education-\d+/, 'education-form' + newIndex));
     }
-});
+    });
     
     $('#education-form-container').append(newEducationForm);
     return false;
@@ -28,12 +29,12 @@ $(document).on('click', '#add-work-experience-form', function() {
     newWorkExperienceForm.find('input').val('');
     newWorkExperienceForm.find('select').val('');
     
-    // Generate a unique prefix for the new formset
+    
     var newPrefix = 'work-experience-' + ($('.work-experience-form').length + 1);
     newWorkExperienceForm.find('input, select').each(function() {
         var name = $(this).attr('name');
         if (name) {
-        // Update the name attribute with the new prefix
+        
         $(this).attr('name', name.replace('work_experience-0', newPrefix));
         }
     });
@@ -50,12 +51,12 @@ $(document).on('click', '#add-skill-form', function() {
     newSkillForm.find('input').val('');
     newSkillForm.find('select').val('');
     
-    // Generate a unique prefix for the new formset
+    
     var newPrefix = 'skill-' + ($('.skill-form').length + 1);
     newSkillForm.find('input, select').each(function() {
     var name = $(this).attr('name');
     if (name) {
-        // Update the name attribute with the new prefix
+        
         $(this).attr('name', name.replace('skill-0', newPrefix));
         }   
     });
